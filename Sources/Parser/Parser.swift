ceb491f95b4372488c88e5c7951939dfe09d320f
LiarPrincess <4982138+LiarPrincess@users.noreply.github.com>
2019-04-03 23:03:29 +0200

    9.4

--- Sources/Parser/Parser.swift
+++ Sources/Parser/Parser.swift
@@ -86,6 +86,10 @@ class Parser {
       return try self.whileStatement()
     }
 
+    if self.match(.for) {
+      return try self.forStatement()
+    }
+
     return try self.expressionStatement()
   }
 
@@ -130,6 +134,56 @@ class Parser {
 
     let body = try self.statement()
     return WhileStmt(condition: condition, body: body)
+  }
+
+  private func forStatement() throws -> Stmt {
+    try self.consumeOrThrow(type: .leftParen, error: .missingToken("'('"))
+    let initializer = try self.forStatementInitializer()
+    let condition = try self.forStatementCondition()
+    let increment = try self.forStatementIncrement()
+    let body = try self.statement()
+
+    //  {
+    //    var i = 0;
+    //    while (i < 10) {
+    //      print i;
+    //      i = i + 1;
+    //    }
+    //  }
+
+    var result = body
+    if let increment = increment {
+      result = BlockStmt(statements: [result, ExpressionStmt(expr: increment)])
+    }
+
+    let whileCondition = condition ?? BoolExpr(value: true)
+    result = WhileStmt(condition: whileCondition, body: result)
+
+    if let initializer = initializer {
+      result = BlockStmt(statements: [initializer, result])
+    }
+
+    return result
+  }
+
+  private func forStatementInitializer() throws -> Stmt? {
+    if self.match(.semicolon) { return nil }
+    if self.match(.var) { return try self.varDeclaration() }
+    return try self.expressionStatement()
+  }
+
+  private func forStatementCondition() throws -> Expr? {
+    if self.match(.semicolon) { return nil }
+    let expr = try self.expression()
+    try self.consumeOrThrow(type: .semicolon, error: .missingToken("';'"))
+    return expr
+  }
+
+  private func forStatementIncrement() throws -> Expr? {
+    if self.match(.rightParen) { return nil }
+    let expr = try self.expression()
+    try self.consumeOrThrow(type: .rightParen, error: .missingToken("')'"))
+    return expr
   }
 
   private func expressionStatement() throws -> Stmt {
